43d2fbac4f8f04df9e10249c4303a0bc14dbe511
92664 <92664@farnborough.ac.uk>
2020-11-27 16:02:18 +0000

    buttons work fully

--- resources/client/js/signup.js
+++ resources/client/js/signup.js
@@ -1,27 +1,21 @@
 function AddUser() {
-    //debugger;
-    console.log("Invoked UsersSignup() ");
-
-    let url = "/users/login";
-    window.open("signup.html", "_self");
-    function AddUser() {
-        console.log("Invoked AddUser()");
-        const formData = new FormData(document.getElementById('InputUserDetails'));
-        let url = "/users/add";
-        fetch(url, {
-            method: "POST",
-            body: formData,
-        }).then(response => {
-            return response.json()
-        }).then(response => {
-            if (response.hasOwnProperty("Error")) {
-                alert(JSON.stringify(response));
-            } else {
-                window.open("/client/misc.html", "_self");   //URL replaces the current page.  Create a new html file
-            }                                                  //in the client folder called index.html
-        });
-    }
+    console.log("Invoked AddUser()");
+    const formData = new FormData(document.getElementById('LoginForm'));
+    let url = "/users/add";
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json()
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));
+        } else {
+            window.open("/client/login.html", "_self");   //URL replaces the current page.  Create a new html file
+        }                                                  //in the client folder called index.html
+    });
 }
+
 function UsersSignup() {
     //debugger;
     console.log("Invoked SignUp() ");
