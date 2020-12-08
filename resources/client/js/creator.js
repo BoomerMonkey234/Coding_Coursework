cffcae5fba6ce2219b1383181e3671a63ddf3355
92664 <92664@farnborough.ac.uk>
2020-12-08 10:48:23 +0000

    Signed in as works

--- resources/client/js/creator.js
+++ resources/client/js/creator.js
@@ -3,3 +3,25 @@ function Home() {
     console.log("Invoked ReturnHome() ");
     window.open("index.html", "_self");
 }
+// adds songs
+function addSong() {
+
+    let manufacturer = prompt("add song");
+
+    let formData = new FormData();
+    formData.append("name", Song);
+
+    if (Song != null) {
+        fetch('/song/new', {method: 'post', body: formData}
+        ).then(response => response.json()
+        ).then(data => {
+                if (data.hasOwnProperty('error')) {
+                    alert(data.error);
+                } else {
+                    loadSong();
+                }
+            }
+        );
+    }
+
+}
